14d96c23113bbabc826b8daa43103479fc43c37c
faldyif <faldy.if@gmail.com>
2016-07-02 04:47:13 +0700

    Fix maps fragment

---
+++ app/src/main/java/id/ristech/binradar/Trash.java
@@ -0,0 +1,7 @@
+package id.ristech.binradar;
+
+/**
+ * Created by Faldy on 7/1/2016.
+ */
+public class Trash {
+}
---
+++ app/src/main/java/id/ristech/binradar/UserCheckActivity.java
@@ -0,0 +1,12 @@
+package id.ristech.binradar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserCheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
